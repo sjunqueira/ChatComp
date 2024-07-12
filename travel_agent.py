f0f4fdd03cd3d7661fd8b1677f5dd52ac2c06201
sjunqueira <sergiojunqueira.s@gmail.com>
2024-07-12 19:48:33 -0300

    Migrating aggent to ReAct Agent

--- travel_agent.py
+++ travel_agent.py
@@ -1,27 +1,21 @@
 import os
 from langchain_openai import ChatOpenAI
 from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
 
-from langchain.agents.initialize import initialize_agent
+# Definindo o modelo e as ferramentas
+model = ChatOpenAI(model="gpt-3.5-turbo")
+tools = load_tools(['ddg-search', 'wikipedia'], llm=model)
+prompt = hub.pull("hwchase17/react")
 
-
-llm = ChatOpenAI(model="gpt-3.5-turbo")
-
-tools = load_tools(['ddg-search', 'wikipedia'], llm=llm)
-
-#print(tools[1].name, tools[1].description)
-
-agent = initialize_agent(
-    tools,
-    llm,
-    verbose = True
-)
-
-#print(agent.agent.llm_chain.prompt.template)
-
+# Definindo a consulta
 query = """
 Vou viajar para o Canadá em Outubro de 2024, quero um cronograma completo, de custos de passagens de avião de Florianópolis até Vancouver até passeios e eventos que irão ocorrer em Outubro e seus custos. A viagem será feita em duas pessoas adultas casadas.
 """
+# Criando o agente
+agent = create_react_agent(model, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-
-agent.run(query)
+# Executando a consulta
+agent_executor.invoke({"input": query})
